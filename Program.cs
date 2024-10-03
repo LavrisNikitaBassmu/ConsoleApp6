5e0987c8a4fd9ade9334d1256de8c18fa7a0a501
Admin <Admin@WIN-0QFN66U68EP>
2024-10-03 18:05:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SwapVariables
+{
+    static void Main(string[] args)
+    {
+        // Инициализируем переменные
+        int a = 5;
+        int b = 10;
+
+        Console.WriteLine("До обмена:");
+        Console.WriteLine($"a = {a}, b = {b}");
+
+        // Обменяем значения переменных
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine("После обмена:");
+        Console.WriteLine($"a = {a}, b = {b}");
+    }
+}
